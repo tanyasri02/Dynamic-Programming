5bd772ca3b6ccfb3b1cd039e59f79ba82bd8e35f
tanya srivastava <59734200+tanyasri02@users.noreply.github.com>
2021-07-21 16:19:39 +0530

    Update 0-1 KnapSack.cpp

--- 0-1 KnapSack.cpp
+++ 0-1 KnapSack.cpp
@@ -19,13 +19,14 @@ class Solution
               if(i==0||j==0)
               dp[i][j]=0;
               
-              else if(wt[i-1]>j) dp[i][j]=dp[i-1][j];
+              else if(wt[i-1]>j) 
+                  dp[i][j]=dp[i-1][j];
+              
               else
               dp[i][j]=max(val[i-1]+
                             dp[i-1][j-wt[i-1]],
                             dp[i-1][j]);
-            //  else
-            //   dp[i][j]=dp[i-1][j];
+            
           }
       }
       return dp[n][W];
